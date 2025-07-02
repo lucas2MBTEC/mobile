59b56201b02ab26395f50331c4e2e2d6112bbf7f
Lucas araújo <lucas.zimovski.brasil@escola.pr.gov.br>
2025-07-02 08:21:39 -0300

    pato

---
+++ aula01/app.java
@@ -0,0 +1,13 @@
+package aula01;
+
+public class app{
+
+String nome;
+String descricao;
+String autor;
+String isbn;
+double preco;
+String datapub;
+
+}
+
---
+++ aula01/aula.java
@@ -0,0 +1,19 @@
+package aula01;
+
+public class aula {
+    public static void main(String[] args) {
+        app livro = new app();
+livro.nome = "Bíbilia Sagrada";
+livro.descricao = "conta história de Jesus";
+livro.autor = "Padres e tradutores";
+livro.isbn = "9784820211396";
+livro.preco = 40;
+livro.datapub = "100 d.C";
+
+System.out.println(livro.nome);
+
+
+
+
+    }
+}
